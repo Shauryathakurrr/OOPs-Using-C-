ab99c0bea9fc303383e3be81da991443167ae9c7
shauryathakurrr <shauryathakur1981@gmail.com>
2026-02-17 12:31:39 +0530

    constructor of all type

---
+++ constructors.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class constructor
+{
+public:
+    
+
+    constructor()
+    {
+        cout << "deafult constructor worked..." << endl;
+    }
+
+   
+    constructor(int a)
+    {
+        cout << "parameterized constructor worked..." << endl;
+        cout << "Value is: " << a << endl;
+    }
+
+
+    constructor(const constructor &obj)
+    {
+        cout << "copy constructor worked..." << endl;
+    }
+};
+
+int main()
+{
+    constructor obj1;       
+    constructor obj2(21);     
+    constructor obj3(obj2);  
+
+    return 0;
+}
